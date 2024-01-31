d258be31f020593b9a77f3ae6806f9b5180cf7d1
Joshua Ang <joshua.ang.sw@gmail.com>
2024-01-31 16:00:53 +0800

    add two more data types - lyrics and memes

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const quotes = ['welcome to the real world! It sucks. You\'re gonna love it.', 'We were on a break!', 'Could I be wearing any more clothes?','She\'s your lobster','Can I interest you in a sarcastic comment?']
 
+const lyrics = ['I watch that pretty life play out in pictures from afar', 'Just keep me where the light is', 'Just a great figure eight or a tiny infinity', 'Can\'t seem to shake these vultures off of my trail', 'And I\'ll be dreamin of the next time we can go into another serotonin overflow']
+const meme =['pepeLaugh', 'monkaS', 'pogChamp', 'kekW', 'pauseChamp']
+const nestedArr = [quotes, lyrics, meme]
+
 function randomQuote(arr) {
-    quoteIndex = Math.floor(Math.random() * arr.length);
-    return arr[quoteIndex];
+    const arrIndex = Math.floor(Math.random() * arr.length);
+        let arrCategory = arr[arrIndex]
+        const quoteIndex = Math.floor(Math.random() * arrCategory.length)
+        return arrCategory[quoteIndex]
+
 }
 
 
-console.log(randomQuote(quotes))
-
-   
+console.log(randomQuote(nestedArr))
