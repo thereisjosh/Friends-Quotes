795171e2304791e6307475a62f22459220215673
Joshua Ang <joshua.ang.sw@gmail.com>
2024-01-31 17:32:09 +0800

    add functionality to log different messages

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-const quotes = ['welcome to the real world! It sucks. You\'re gonna love it.', 'We were on a break!', 'Could I be wearing any more clothes?','She\'s your lobster','Can I interest you in a sarcastic comment?']
 
-const lyrics = ['I watch that pretty life play out in pictures from afar', 'Just keep me where the light is', 'Just a great figure eight or a tiny infinity', 'Can\'t seem to shake these vultures off of my trail', 'And I\'ll be dreamin of the next time we can go into another serotonin overflow']
-const meme =['pepeLaugh', 'monkaS', 'pogChamp', 'kekW', 'pauseChamp']
-const nestedArr = [quotes, lyrics, meme]
+const arrObject = {
+ quotes: ['welcome to the real world! It sucks. You\'re gonna love it.', 'We were on a break!', 'Could I be wearing any more clothes?','She\'s your lobster','Can I interest you in a sarcastic comment?'],
+ lyrics: ['I watch that pretty life play out in pictures from afar', 'Just keep me where the light is', 'Just a great figure eight or a tiny infinity', 'Can\'t seem to shake these vultures off of my trail', 'And I\'ll be dreamin of the next time we can go into another serotonin overflow'],
+ meme: ['pepeLaugh', 'monkaS', 'pogChamp', 'kekW', 'pauseChamp']
+}
+
+let newArr = [];
+
+for (let category in arrObject) {
+    const quoteIndex = Math.floor(Math.random() * arrObject[category].length)
+
+    switch(category) {
+        case 'quotes':
+            newArr.push(`Quote: ${arrObject[category][quoteIndex]}`)
+            break;
+        case 'lyrics':
+            newArr.push(`Lyric: ${arrObject[category][quoteIndex]}`)
+            break;
+        case 'meme':
+            newArr.push(`Meme: ${arrObject[category][quoteIndex]}`)
+            break;
+        default: 
+            newArr.push('There is not enough info.')
+            break;
+    }
+}
 
 function randomQuote(arr) {
-    const arrIndex = Math.floor(Math.random() * arr.length);
-        let arrCategory = arr[arrIndex]
-        const quoteIndex = Math.floor(Math.random() * arrCategory.length)
-        return arrCategory[quoteIndex]
-
+    const formatted = arr.join('\n')
+    console.log(formatted);
 }
 
 
-console.log(randomQuote(nestedArr))
+
+randomQuote(newArr)
